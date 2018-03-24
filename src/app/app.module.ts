69bf8cdbd2d41f9daedb8f6215cee89692f6413d
Manolo Torres <=>
2018-03-24 18:45:42 +0100

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,25 @@ import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
 
 import { DataService } from './services/data.service';
-import { HttpModule } from '@angular/http'
+import { HttpModule } from '@angular/http';
+import { AboutComponent } from './components/about/about.component';
+import { RouterModule, Routes} from '@angular/router';
 
+const appRoutes: Routes = [
+  {path: '', component:UserComponent},
+  {path: 'about', component:AboutComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
