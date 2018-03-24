4c43eb2c4077f33c9dceec2d3e70aac3d7089f5e
Manolo Torres <=>
2018-03-24 13:26:38 +0100

    Presentación condicional

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -13,6 +13,7 @@ export class UserComponent implements OnInit {
   address: Address;
   hobbies: string[];
   posts: Post[];
+  isEdit: boolean = false;
 
   constructor(private dataService: DataService) {   }
 
@@ -41,6 +42,10 @@ export class UserComponent implements OnInit {
         this.hobbies.splice(i, 1);
       }
     }
+  }
+
+  toggleEdit() {
+    this.isEdit = !this.isEdit;
   }
 
 }
